4c18eb77f51dadf46b8e9ee1619963b10427e164
escrib <sebascraton@gmail.com>
2023-04-02 16:19:41 -0300

    Api

---
+++ assets/js/api.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {  
+    $.ajax({  
+        type: "GET",  
+        url: "https://api.bluelytics.com.ar/v2/latest",    
+        dataType: "json", 
+    }).done((data) => {
+        let dol = data.blue.value_avg;
+            var row = $('<tr>');
+            row.append($(`<th> 1 u$s </th>`));
+            row.append($(`<th>$ ${ dol }</th>`));
+            $('#tabla tbody').append(row);}
+    );
+})
