5c7e4d9e415d9a9b2bc160451a0f7132827b5bf7
escrib <sebascraton@gmail.com>
2023-04-04 16:14:19 -0300

    validate y exportar pdf para implementar 2

--- implementacion-jsPDF/jspdf.js
+++ implementacion-jsPDF/jspdf.js
@@ -7,10 +7,10 @@ function crearPdf() {
   paso3 = document.getElementById("paso3").value;
   paso4 = document.getElementById("paso4").value;
 
-  doc.text(10, 30, paso1);
-  doc.text(10, 30, paso2);
+  doc.text(10, 10, paso1);
+  doc.text(10, 20, paso2);
   doc.text(10, 30, paso3);
-  doc.text(10, 30, paso4);
+  doc.text(10, 40, paso4);
 
   doc.save('Resumen.pdf');
 }
