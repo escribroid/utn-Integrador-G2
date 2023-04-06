c4c69080c68aa33d9a6afd155956ead918517b76
escrib <sebascraton@gmail.com>
2023-04-05 23:26:22 -0300

    Validacion form

---
+++ assets/js/pdf.js
@@ -0,0 +1,16 @@
+window.jsPDF = window.jspdf.jsPDF;
+
+var doc = new jsPDF();
+function crearPdf() {
+  paso1 = document.getElementById("paso1").value;
+  paso2 = document.getElementById("paso2").value;
+  paso3 = document.getElementById("paso3").value;
+  paso4 = document.getElementById("paso4").value;
+
+  doc.text(10, 10, paso1);
+  doc.text(10, 20, paso2);
+  doc.text(10, 30, paso3);
+  doc.text(10, 40, paso4);
+
+  doc.save('Resumen.pdf');
+}
---
+++ assets/js/validador.js
@@ -0,0 +1,47 @@
+$(function() {
+    $("form[name='contacto']").validate({
+      rules: {
+        nombre: {
+            required: true,
+            minlength: 3,
+            maxlength: 50,
+          },
+        correo: {
+          required: true,
+          presence: true,
+          email: true,
+          minlength: 3,
+          maxlength: 50,
+        },
+        telefono: {
+          required: true,
+          minlength: 5,
+          maxlength: 15,
+          digits: true,
+
+        }
+      },
+      messages: {
+        nombre: {
+            required: "Por favor, introduzca su nombre y apellido",
+            minlength: "Su contraseña debe tener entre 3 y 50 caracteres.",
+            maxlength: "Su contraseña debe tener entre 3 y 50 caracteres.",
+          },
+        telefono: {
+          required: "Por favor proporcione un telefono",
+          minlength: "Debe tener entre 5 y 15 caracteres.",
+          maxlength: "Debe tener entre 5 y 15 caracteres.",
+          digits: "Ingrese solo numeros",
+
+        },
+        correo: {
+            required: "Por favor ingrese un correo electronico",
+            email: "Introduce correo electrónico válido de hasta 50 caracteres",
+            length: "Su contraseña debe tener al menos 55 caracteres."
+          },
+      },
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
