d3480c118dcd60b8ef657bdcf13d723dca612a52
Samuel Calderon <calderon.samuel@gmail.com>
2023-04-07 17:06:03 -0300

    se incluye tengnologia html, css

---
+++ samuel/tecnologias.js
@@ -0,0 +1,20 @@
+body{
+    background-color: #EFEFEF;
+}
+.titulo_tec{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: blueviolet;
+    text-align: center;
+    font-size: 35px;   
+}
+.cuerpo_tec
+{
+    text-align: center;
+    font-family: fantasy;
+}
+.footer_imag{
+    position: relative;
+    top: 120px;
+    display: flex;
+    justify-content: space-between;
+}
