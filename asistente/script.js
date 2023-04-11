baa3f5d8fed74043666f00d345accb00eb48704e
Pax <ojosperla@gmail.com>
2023-04-11 12:39:45 -0300

    Orden de archivos y clases en Asistente.

--- asistente/script.js
+++ asistente/script.js
@@ -1,4 +1,3 @@
-
 const movPag = document.querySelector(".movPag");
 const pagina2 = document.querySelector(".pagina2");
 const BotonAvance1 = document.querySelector(".avanzarpag1");
@@ -9,34 +8,32 @@ const BotonAvance2 = document.querySelector(".avanzarpag2");
 // const step2 = document.querySelector("#pagina-2");
 // const summary = document.querySelector("#summary");
 
-BotonAvance1.addEventListener("click", function() {
+BotonAvance1.addEventListener("click", function () {
   // recopilar datos del primer paso
   let name = document.getElementById("nombre-empresa").value;
-    alert ("Hola su empresa es "+name);
+  //alert("Hola su empresa es " + name);
   let tipo_web = document.querySelector("#web").value;
-  let fechaentrega= document.getElementById("fecha_entrega").value;
-  alert("Ya agendamos el tipo de desarrollo que desea y la fecha");
-  movPag.style.display= "none"; 
-  pagina2.style.display="block";  
+  let fechaentrega = document.getElementById("fecha-entrega").value;
+  //alert("Ya agendamos el tipo de desarrollo que desea y la fecha");
+  movPag.style.display = "none";
+  pagina2.style.display = "block";
 });
 
-BotonAvance2.addEventListener("click", function() {
-    // tomo info de los input
-    let nombreyapellido = document.getElementById("nombreyapellido").value;
-    alert ("Le responderemos a la brevedad. gracias a ud: "+nombreyapellido);
-   let mail= document.getElementById("email").value;
-   alert ("Le esceibiremos a este mail "+mail);
-   pagina2.style.display="none";
-  
-    //falta mostrar resumen en el segundo paso
-    // ver.....summary.innerHTML = `Nombre: ${nombreyapellido}<br>Email: ${email}`;
-  });
-  
-  BotonVolver0.addEventListener("click", function() {
-    // tomo info de los input
-    pagina2.style.display= "none"; 
-    movPag.style.display="block";
-  
-    // ocultar primer pagina y mostrar la segunda 
-  });
-  
+BotonAvance2.addEventListener("click", function () {
+  // tomo info de los input
+  let nombreyapellido = document.getElementById("nombreyapellido").value;
+  let mail = document.getElementById("email").value;
+  alert("Le responderemos a la brevedad. Muchas gracias, " + nombreyapellido + "Le responderemos al email " + mail);
+  pagina2.style.display = "none";
+
+  //falta mostrar resumen en el segundo paso
+  // ver.....summary.innerHTML = `Nombre: ${nombreyapellido}<br>Email: ${email}`;
+});
+
+BotonVolver0.addEventListener("click", function () {
+  // tomo info de los input
+  pagina2.style.display = "none";
+  movPag.style.display = "block";
+
+  // ocultar primer pagina y mostrar la segunda 
+});
