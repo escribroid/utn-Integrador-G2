9dd80e193025489439e0655af4f0fae84d6ea044
escrib <sebascraton@gmail.com>
2023-04-12 00:20:01 -0300

    asistente algo, funcionalidad pdf

--- asistente/script.js
+++ asistente/script.js
@@ -1,7 +1,11 @@
 const movPag = document.querySelector(".movPag");
+const pagina = document.querySelector(".pagina");
 const pagina2 = document.querySelector(".pagina2");
+const pagina3 = document.querySelector(".pagina3");
+
 const BotonAvance1 = document.querySelector(".avanzarpag1");
 const BotonVolver0 = document.querySelector(".retrocederpag0");
+const BotonVolver1 = document.querySelector(".retrocederpag1");
 const BotonAvance2 = document.querySelector(".avanzarpag2");
 
 // const step1 = document.querySelector("#pagina");
@@ -9,37 +13,41 @@ const BotonAvance2 = document.querySelector(".avanzarpag2");
 // const summary = document.querySelector("#summary");
 
 BotonAvance1.addEventListener("click", function () {
-  // recopilar datos del primer paso
-  let name = document.getElementById("nombre-empresa").value;
-  // alert("Hola su empresa es " + name);
-
-  let tipo_web = document.getElementById("web").value;
-  // let tipo_web = document.querySelector("#web").value;
-  let fechaentrega = document.getElementById("fecha-entrega").value;
-  // alert("Ya agendamos el tipo de desarrollo que desea y la fecha");
-  movPag.style.display = "none";
-  pagina2.style.display = "block";
+    // recopilar datos del primer paso
+    // alert("Hola su empresa es " + name);
+    // let tipo_web = document.querySelector("#web").value;
+    // alert("Ya agendamos el tipo de desarrollo que desea y la fecha");
+    movPag.style.display = "none";
+    pagina2.style.display = "block";
 });
 
 BotonAvance2.addEventListener("click", function () {
-  // tomo info de los input
-  let nombreyapellido = document.getElementById("nombreyapellido").value;
-  let mail = document.getElementById("email").value;
-  alert("Le responderemos a la brevedad. Muchas gracias, " + nombreyapellido + ". Le responderemos al email " + mail);
-  pagina2.style.display = "none";
-
-  //falta mostrar resumen en el segundo paso
-  // ver.....summary.innerHTML = `Nombre: ${nombreyapellido}<br>Email: ${email}`;
+    // tomo info de los input
+    let nombreEmpresa = document.getElementById("nombre-empresa").value;
+    let fechaEntrega = document.getElementById("fecha-entrega").value;
+    let nombreyapellido = document.getElementById("nombreyapellido").value;
+    let mail = document.getElementById("email").value;
+    movPag.style.display = "none";
+    pagina.style.display = "none";
+    pagina2.style.display = "none";
+    pagina3.style.display = "block";
+    document.querySelector(".span1").innerHTML = nombreEmpresa;
+    document.querySelector(".span2").innerHTML = fechaEntrega;
+    document.querySelector(".span3").innerHTML = nombreyapellido;
+    document.querySelector(".span4").innerHTML = mail;
 });
 
 BotonVolver0.addEventListener("click", function () {
-  // tomo info de los input
-  pagina2.style.display = "none";
-  movPag.style.display = "block";
+    // tomo info de los input
+    pagina2.style.display = "none";
+    movPag.style.display = "block";
 
-  // ocultar primer pagina y mostrar la segunda 
+    // ocultar primer pagina y mostrar la segunda
 });
-// otra opcion para mostrar lo que hay en el select
-// var x = document.getElementById("web").selectedIndex;
-    // var y = document.getElementById("web").options;
-    // alert("Index: " + y[x].index + " is " + y[x].text);
+BotonVolver1.addEventListener("click", function () {
+    // tomo info de los input
+    pagina2.style.display = "block";
+    pagina3.style.display = "none";
+
+    // ocultar primer pagina y mostrar la segunda
+});
